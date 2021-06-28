a642e5d8ca6ab197ecae20c51d503ed0b1d57554
Junior, L F Nunes <38589846+lfnjunior@users.noreply.github.com>
2021-06-28 18:53:36 -0300

    lib html to pdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,180 +1,36 @@
+import { Preview, print } from "react-html2pdf";
 import "./styles.css";
 
 export default function App() {
-  function printTest() {
-    window.print();
-  }
-
   return (
     <>
-      <div id="not-print" className="App">
-        <h1>Hora de imprimir</h1>
-        <button onClick={printTest}>Imprimir</button>
-      </div>
-      <div id="print" className="App">
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-        <h1>Hora de imprimir</h1>
-      </div>
+      <Preview id={"jsx-template"}>
+        <div>
+          <div
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              display: "grid",
+              gridTemplateColumns: "20% 60% 20%",
+              height: "100px"
+            }}
+          >
+            <div>
+              <h1>1</h1>
+            </div>
+            <div>
+              <div>
+                Transportador: 49.871.213/0001-88 - IC TRANSPORTES LTDA.
+                SUMARÉ-SP
+              </div>
+            </div>
+            <div>
+              <h1>3</h1>
+            </div>
+          </div>
+        </div>
+      </Preview>
+      <button onClick={() => print("a", "jsx-template")}> print</button>
     </>
   );
 }
